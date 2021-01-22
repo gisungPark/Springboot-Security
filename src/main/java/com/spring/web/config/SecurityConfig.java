53c20cbbec4f6534c9327e2a28a6c50375977c41
gisung2215 <wjv2215@naver.com>
2021-01-22 21:30:21 +0900

    feat: 스프링 시큐리티를 이용한 인증 및 회원가입 구현

---
+++ src/main/java/com/spring/web/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.spring.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity // 필터 체인 관리 시작 어노테이션
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	// 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
+	@Bean
+	public BCryptPasswordEncoder encodePwd() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/user/**").authenticated()
+			.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/loginForm");
+	}
+}
--- src/main/java/com/spring/web/controller/IndexController.java
+++ src/main/java/com/spring/web/controller/IndexController.java
@@ -1,15 +1,67 @@
 package com.spring.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.spring.web.model.User;
+import com.spring.web.repository.UserRepository;
 
 @Controller
 public class IndexController {
-
-	@GetMapping({"", "/"})
+	
+	@Autowired 
+	private UserRepository userRepository;
+	
+	@Autowired 
+	private BCryptPasswordEncoder bCryptPasswordEncoder; 
+	
+	@GetMapping("/")
 	public String index() {
 		//머스테치 기본폴더 src/main/resources/
 		//뷰리졸버 설정: template(prefix), .mustache(suffix)
 		return "index";
 	}
+	
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "manager";
+	}
+	
+	// 스프링시큐리티가 해당주소를 낚아챔 - SecurityConfig 파일 생성 후 비활성화
+	@GetMapping("/loginForm")
+	public String login() {
+		return "loginForm";
+	}
+	
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
+	}
+	
+	@PostMapping("/join")
+	public String join(User user) {
+		user.setRole("ROLE_USER");
+		String rawPassword = user.getPassword();
+		String encPasswrod = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPasswrod);
+		userRepository.save(user);	// 회원가입 잘됨. 비밀번호 1234
+									// but, 시큐리티로 로그인을 할 수 없음.
+									// 이유는 패스워드가 암호화가 안되었기 때문!!
+		return "redirect:/loginForm";
+	}
+	
 }
